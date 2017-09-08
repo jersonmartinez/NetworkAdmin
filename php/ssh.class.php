07f8bb83b1911180fd8d11daf35687fec62074c9
Side Master <sidemasterdelfuturo@gmail.com>
2017-09-07 21:25:01 -0600

    Acciones agregadas para DHCP, DNS y HTTP

--- php/ssh.class.php
+++ php/ssh.class.php
@@ -10,6 +10,7 @@
 
 		private $local_path = "/var/www/html/NetworkAdmin/php/";
 		private $remote_path;
+		private $filename;
 
 		function __construct($ip_host, $username, $password){
 			if (!function_exists("ssh2_connect")) 
@@ -42,39 +43,227 @@
             	return "<pre>".$data."</pre>";
 		}
 
-		public function getDHCP(){
-			$RL[] = "ls -l"; 
-			array_push($RL, "cat /etc/network/interfaces");
-			
-			return $this->RunLines(implode("\n", $RL));
-		}
-
-		public function getDHCPMemState(){
-			$filename = "getDHCPMemState.sh";
-			$ActionArray[] = "MEMORIA=($(free -m | grep 'Mem' | cut -d ':' -f2))";
-			array_push($ActionArray, 'echo "${MEMORIA[0]},${MEMORIA[1]},${MEMORIA[2]},${MEMORIA[3]},${MEMORIA[4]},${MEMORIA[5]},"');
-			
-			$inputfile = file_put_contents("./".$filename, implode("\n", $ActionArray));
+		public function writeFile($Instructions, $filename){
+			$inputfile = file_put_contents($this->local_path.$filename, implode("\n", $Instructions));
 
 			if ($inputfile === false)
 				die("El script <b>".$filename."</b>, no se ha podido crear.");
 
-			@chmod($filename, 0777);
+			@chmod($this->local_path.$filename, 0777);
+		
+			return true;
+		}
 
-			$scp = ssh2_scp_send($this->connect, "./".$filename, "/home/".$this->username."/".$filename, 0777);
+		public function sendFile($filename){
+			$scp = ssh2_scp_send($this->connect, $this->local_path.$filename, $this->remote_path.$filename, 0777);
 
 			if (!$scp)
 				die("Error al intentar enviar el script <b>".$filename."</b> al host con IP <b>".$this->ip_host."</b>");
 
-			if (!unlink($filename))
+			if (!unlink($this->local_path.$filename))
 				die("El script <b>".$filename."</b> no se ha podido eliminar del sistema local.");
 
+			return true;
+		}
+
+		public function getMemoryState(){
+			$filename = "getMemoryState.sh";
+			$ActionArray[] = "MEMORIA=($(free -m | grep 'Mem' | cut -d ':' -f2))";
+			array_push($ActionArray, 'echo "${MEMORIA[0]},${MEMORIA[1]},${MEMORIA[2]},${MEMORIA[3]},${MEMORIA[4]},${MEMORIA[5]},"');
+			
 			$RL[] = $this->remote_path.$filename;
 			array_push($RL, "rm -rf ".$this->remote_path.$filename);
 
-			return $this->RunLines(implode("\n", $RL));
+			if ($this->writeFile($ActionArray, $filename) && $this->sendFile($filename))
+				return $this->RunLines(implode("\n", $RL));
+
+			return false;
+		}
+
+		public function getDiskUsage(){
+			$filename = "getDiskUsage.sh";
+
+			$ActionArray[] = "INTERFACES=($(ifconfig -a -s | awk {'print $1'}))";
+			array_push($ActionArray, 'echo "="');
+			array_push($ActionArray, 'NUM_INTER=${#INTERFACES[*]}');
+			array_push($ActionArray, 'for (( i = 1; i < $NUM_INTER ; i++ )); do');
+			array_push($ActionArray, '	DIRECCION_IP=$(ifconfig ${INTERFACES[$i]} | grep "inet " | cut -d " " -f10)');
+			array_push($ActionArray, '	if [[ $DIRECCION_IP != "" ]]; then');
+			array_push($ActionArray, '		echo "${INTERFACES[$i]},$DIRECCION_IP,"');
+			array_push($ActionArray, '	else');
+			array_push($ActionArray, '		echo "${INTERFACES[$i]},No tiene ip asignada"');
+			array_push($ActionArray, '	fi');
+			array_push($ActionArray, 'done');
+			array_push($ActionArray, 'echo "="');
+			
+			$RL[] = $this->remote_path.$filename;
+			array_push($RL, "rm -rf ".$this->remote_path.$filename);
+
+			if ($this->writeFile($ActionArray, $filename) && $this->sendFile($filename))
+				return $this->RunLines(implode("\n", $RL));
+
+			return false;
+		}
+
+		public function getNetworkInterfaces(){
+			$filename = "getNetworkInterfaces.sh";
+
+			$ActionArray[] = "INTERFACES=($(ifconfig -a -s | awk {'print $1'}))";
+			array_push($ActionArray, 'echo "="');
+			array_push($ActionArray, 'NUM_INTER=${#INTERFACES[*]}');
+			array_push($ActionArray, 'for (( i = 1; i < $NUM_INTER ; i++ )); do');
+			array_push($ActionArray, '	DIRECCION_IP=$(ifconfig ${INTERFACES[$i]} | grep "inet " | cut -d " " -f10)');
+			array_push($ActionArray, '	if [[ $DIRECCION_IP != "" ]]; then');
+			array_push($ActionArray, '		echo "${INTERFACES[$i]},$DIRECCION_IP,"');
+			array_push($ActionArray, "	else");
+			array_push($ActionArray, '		echo "${INTERFACES[$i]},No tiene ip asignada"');
+			array_push($ActionArray, '	fi');
+			array_push($ActionArray, 'done');
+			array_push($ActionArray, 'echo "="');
+			
+			$RL[] = $this->remote_path.$filename;
+			array_push($RL, "rm -rf ".$this->remote_path.$filename);
+
+			if ($this->writeFile($ActionArray, $filename) && $this->sendFile($filename))
+				return $this->RunLines(implode("\n", $RL));
+
+			return false;
+		}
+
+		public function getOpenPorts(){
+			$filename = "getOpenPorts.sh";
+
+			$ActionArray[] = "PORT_TCP=($(netstat -pltona | grep 'tcp ' | awk {'print $4 ,$1'} | cut -d ':' -f2))";
+			array_push($ActionArray, "PORT_TCP6=($(netstat -pltona | grep 'tcp6' | awk {'print $4 ,$1'} | cut -d ':' -f4))");
+			array_push($ActionArray, 'echo "${PORT_TCP[*]} ${PORT_TCP6[*]},"');
+			array_push($ActionArray, "PORT_UDP=($(netstat -pluona | grep 'udp ' | awk {'print $4 ,$1'} | cut -d ':' -f2))");
+			array_push($ActionArray, "PORT_UDP6=($(netstat -pluona | grep 'udp ' | awk {'print $4 ,$1'} | cut -d ':' -f4))");
+			array_push($ActionArray, 'echo "${PORT_UDP[*]} ${PORT_UDP6[*]}"');
+			array_push($ActionArray, 'echo "="');
+			
+			$RL[] = $this->remote_path.$filename;
+			array_push($RL, "rm -rf ".$this->remote_path.$filename);
+
+			if ($this->writeFile($ActionArray, $filename) && $this->sendFile($filename))
+				return $this->RunLines(implode("\n", $RL));
+
+			return false;
+		}
+
+		public function getNetworkConnections(){
+			$filename = "getNetworkConnections.sh";
+
+			$ActionArray[] = "PROTO=$(netstat -putona | grep -e tcp -e udp | awk {'print $1'})";
+			array_push($ActionArray, "DIR_LOCAL=$(netstat -putona | grep -e tcp -e udp | awk {'print $4'})");
+			array_push($ActionArray, "DIR_REMOTA=$(netstat -putona | grep -e tcp -e udp | awk {'print $5'})");
+			array_push($ActionArray, "ESTADO=$(netstat -putona | grep -e tcp -e udp | awk {'print $6'})");
+			array_push($ActionArray, "TEMP1=$(netstat -putona | grep -e tcp -e udp | awk {'print $7'})");
+			array_push($ActionArray, 'echo "${PROTO[*]} | "');
+			array_push($ActionArray, 'echo "${DIR_LOCAL[*]} | "');
+			array_push($ActionArray, 'echo "${DIR_REMOTA[*]} | "');
+			array_push($ActionArray, 'echo "${ESTADO[*]} | "');
+			array_push($ActionArray, 'echo "${TEMP1[*]} | "');
+			array_push($ActionArray, 'echo "="');
+			
+			$RL[] = $this->remote_path.$filename;
+			array_push($RL, "rm -rf ".$this->remote_path.$filename);
+
+			if ($this->writeFile($ActionArray, $filename) && $this->sendFile($filename))
+				return $this->RunLines(implode("\n", $RL));
+
+			return false;
+		}
+
+		public function getUsersConnected(){
+			$filename = "getUsersConnected.sh";
+
+			$ActionArray[] = "USUARIOS=($(who | cut -d ' ' -f1))";
+			array_push($ActionArray, 'for i in ${USUARIOS[*]}; do');
+			array_push($ActionArray, '	echo "$i ,"');
+			array_push($ActionArray, 'done');
+			array_push($ActionArray, 'echo "="');
+			
+			$RL[] = $this->remote_path.$filename;
+			array_push($RL, "rm -rf ".$this->remote_path.$filename);
+
+			if ($this->writeFile($ActionArray, $filename) && $this->sendFile($filename))
+				return $this->RunLines(implode("\n", $RL));
+
+			return false;
+		}
+
+		public function getDHCPShowAssignIP(){
+			$filename = "getDHCPShowAssignIP.sh";
+
+			$ActionArray[] = "MES=$(service isc-dhcp-server status | tail -n10 | grep 'DHCPACK' | awk {'print $1'})";
+			array_push($ActionArray, "DIA=$(service isc-dhcp-server status | tail -n10 | grep 'DHCPACK' | awk {'print $2'})");
+			array_push($ActionArray, "HORA=$(service isc-dhcp-server status | tail -n10 | grep 'DHCPACK' | awk {'print $3'})");
+			array_push($ActionArray, "IP=$(service isc-dhcp-server status | tail -n10 | grep 'DHCPACK' | awk {'print $8'})");
+			array_push($ActionArray, "MAC=$(service isc-dhcp-server status | tail -n10 | grep 'DHCPACK' | awk {'print $10'})");
+			array_push($ActionArray, "INTERFAZ=$(service isc-dhcp-server status | tail -n10 | grep 'DHCPACK' | awk {'print $NF'})");
+			array_push($ActionArray, 'echo "${MES[*]} | "');
+			array_push($ActionArray, 'echo "${DIA[*]} | "');
+			array_push($ActionArray, 'echo "${HORA[*]} | "');
+			array_push($ActionArray, 'echo "${IP[*]} | "');
+			array_push($ActionArray, 'echo "${MAC[*]} | "');
+			array_push($ActionArray, 'echo "${INTERFAZ[*]} | "');
+			array_push($ActionArray, 'echo "="');
+			
+			$RL[] = $this->remote_path.$filename;
+			array_push($RL, "rm -rf ".$this->remote_path.$filename);
+
+			if ($this->writeFile($ActionArray, $filename) && $this->sendFile($filename))
+				return $this->RunLines(implode("\n", $RL));
+
+			return false;
+		}
+
+		public function getDNSFileZones(){
+			$filename = "getDNSFileZones.sh";
+
+			$ActionArray[] = "ZONAS=($(cat /etc/bind/named.conf.local | grep 'file' | awk {'print $2'} | tr -d '\";'))";
+			array_push($ActionArray, 'CANT_ZONAS=${#ZONAS[*]}');
+			array_push($ActionArray, 'for (( i = 0; i < $CANT_ZONAS; i++ )); do');
+			array_push($ActionArray, '	DOMINIO=$(cat ${ZONAS[$i]} | grep "SOA" | awk {"print $4"} | sed "s/.$//g")');
+			array_push($ActionArray, '	TRADUC=$(cat ${ZONAS[$i]} | grep -e "IN" | tail -n1 | awk "! /$DOMINIO/ {print $1}")');
+			array_push($ActionArray, '	IP=$(cat ${ZONAS[$i]} | grep "IN" | tail -n1 | awk "! /$DOMINIO/ {print $4}")');
+			array_push($ActionArray, '	echo " ${ZONAS[$i]},$DOMINIO,${TRADUC[*]}.$DOMINIO,${IP[*]}"');
+			array_push($ActionArray, "done");
+			array_push($ActionArray, 'echo "="');
+			
+			$RL[] = $this->remote_path.$filename;
+			array_push($RL, "rm -rf ".$this->remote_path.$filename);
+
+			if ($this->writeFile($ActionArray, $filename) && $this->sendFile($filename))
+				return $this->RunLines(implode("\n", $RL));
+
+			return false;
+		}
+
+		public function getHTTPVirtualHost(){
+			$filename = "getHTTPVirtualHost.sh";
+
+			$ActionArray[] = "SITIOS=$(ls /etc/apache2/sites-available/)";
+			array_push($ActionArray, 'for i in ${SITIOS[*]}; do');
+			array_push($ActionArray, '	NAME_SERVER=$(cat /etc/apache2/sites-available/$i | grep "ServerName" | cut -d " " -f2 | tail -n1)');
+			array_push($ActionArray, '	SITE_ENABLE=$(ls /etc/apache2/sites-enabled/ | grep $i)');
+			array_push($ActionArray, '	if [[ $SITE_ENABLE == "" && $NAME_SERVER == "" ]]; then');
+			array_push($ActionArray, '		echo "$i,No identificado,No habilitado"');
+			array_push($ActionArray, '	else');
+			array_push($ActionArray, '		echo "$i,$NAME_SERVER,Habilitado"');
+			array_push($ActionArray, '	fi');
+			array_push($ActionArray, 'done');
+			array_push($ActionArray, 'echo "="');
+			
+			$RL[] = $this->remote_path.$filename;
+			array_push($RL, "rm -rf ".$this->remote_path.$filename);
+
+			if ($this->writeFile($ActionArray, $filename) && $this->sendFile($filename))
+				return $this->RunLines(implode("\n", $RL));
+
+			return false;
 		}
 	}
 
-	echo (new ConnectSSH("192.168.100.2", "network", "123"))->getDHCPMemState();
+	echo (new ConnectSSH("192.168.100.4", "network", "123"))->getHTTPVirtualHost();
 ?>
